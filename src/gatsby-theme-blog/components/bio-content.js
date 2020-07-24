360f57219f9109d6b727f61e64fa863116da2e5f
Carlos J. Soto <csoto09@gmail.com>
2020-07-24 18:56:31 -0400

    change bio component

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BioContent = () => (
+  <>
+    Words by Carlos J. Soto.<br />
+    This remains all a bit default and blah.
+  </>
+)
+
+export default BioContent
